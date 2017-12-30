de598dfcb47ef100fb78be897f72486758537062
Mike DePaul <tars@DellRedux.localdomain>
2017-12-29 22:49:34 -0500

    Renamed primary project to just Rando for ease of use and aesthetics

---
+++ Rando/IPrng.cs
@@ -0,0 +1,15 @@
+﻿/* Attribution
+ * Mike DePaul
+ * https://github.com/mdepaul/XRng.git
+ * */
+namespace MD.RandoCalrissian
+{
+    public interface IPrng
+    {
+        byte GetRandomByte();
+
+        byte[] GetRandomBytes(int numberOfBytes);
+
+        void Reseed();
+    }
+}
---
+++ Rando/XEncoding.cs
@@ -0,0 +1,16 @@
+﻿/* Attributions
+ * Mike DePaul
+ * https://github.com/mdepaul/Rando-Calrissian.git
+ * **/
+
+namespace MD.RandoCalrissian
+{
+    public enum XEncoding
+    {
+        Hex,
+        Base64,
+        SafeBase64,
+        Password,
+        Dice
+    }
+}
